5e50e470bab61ea97cf516e21d09aab290cb0eab
tomo-kn <okalemon314@gmail.com>
2022-06-20 02:34:17 +0900

    Fix:本番環境の複数のエラーを解消するため、productionのconfigを修正 #6

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,7 +28,11 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
+  config.assets.js_compressor = :uglifier
+  config.assets.css_compressor = :sass
+  config.assets.compile = false
+  config.assets.digest = true
+  config.assets.resolve_with = %i[manifest]
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = 'http://assets.example.com'
