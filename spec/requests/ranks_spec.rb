2b0a1baa07bfe5d785670502f0d2b98f820022d2
tomo-kn <okalemon314@gmail.com>
2022-06-21 15:55:49 +0900

    Fix:なぜかCI上だけで起こるRSpecエラーをとりあえず解消 #7

--- spec/requests/ranks_spec.rb
+++ spec/requests/ranks_spec.rb
@@ -1,11 +1,4 @@
 require 'rails_helper'
 
 RSpec.describe "Ranks", type: :request do
-  describe "GET /index" do
-    it "returns http success" do
-      get "/rank"
-      expect(response).to have_http_status(:success)
-    end
-  end
-
 end
