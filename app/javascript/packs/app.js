964bed9dd901699ffd6993ae487ed2a7ce466714
tomo-kn <okalemon314@gmail.com>
2022-07-05 13:24:42 +0900

    Feat:録音・再生・やり直し機能の完成 #19

--- app/javascript/packs/app.js
+++ app/javascript/packs/app.js
@@ -2,18 +2,20 @@ const recognition = new webkitSpeechRecognition();
 
 const rec = document.getElementById('rec');
 const notice = document.getElementById('notice');
-// const result = document.getElementById('result');
+const result = document.getElementById('result');
+const playback = document.getElementById('playback');
+const play = document.getElementById("play")
+const restart = document.getElementById('restart');
+const stop = document.getElementById('stop');
+const download = document.getElementById('download');
 
 const text = document.getElementById('text');
-const speeching = document.getElementById('speeching');
+
 var recording = false;
 
 recognition.lang = 'ja';
 recognition.interimResults = true;
 recognition.continuous = false;
-
-const sound = document.getElementById('sound');
-var mediaRecorder;
 
 // for audio
 let audio_sample_rate = null;
@@ -21,11 +23,10 @@ let audioContext = null;
 let scriptProcessor = null;
 let mediaStreamSource = null;
 
-
 // audio data
 let audioData = [];
 let bufferSize = 1024;
-
+let micBlobUrl = null;
 
 let nowRecordingMessage = () => {
   notice.innerHTML = '～録音待機中～';
@@ -48,7 +49,7 @@ var hundleSuccess = (function(stream){
   audioContext = new AudioContext();
   audio_sample_rate = audioContext.sampleRate;
   scriptProcessor = audioContext.createScriptProcessor(bufferSize, 1, 1);
-  mediaStreamSource = audioContext.createMediaStreamSource;
+  mediaStreamSource = audioContext.createMediaStreamSource(stream);
   mediaStreamSource.connect(scriptProcessor);
   scriptProcessor.onaudioprocess = onAudioProcess;
   scriptProcessor.connect(audioContext.destination)
@@ -62,9 +63,6 @@ var hundleSuccess = (function(stream){
   recognition.onspeechend = function() {
     stop.click();
   }
-}).catch(function (error) { // error
-  console.error('mediaDevice.getUserMedia() error:', error);
-  return;
 });
 
 // save audio data //1024bitのバッファサイズに達するごとにaudioDataにデータを追加する
@@ -88,6 +86,92 @@ stop.onclick = function() {
   saveAudio();
 };
 
+// 再生
+play.onclick = function(audioBlob) {
+  if (micBlobUrl) {
+    playback.src = micBlobUrl;
+    // 再生終了時
+    playback.onended = function() {
+      playback.pause();
+      playback.src = '';
+    };
+    // 再生
+    playback.play();
+  }
+};
+
+
+//WAVに変換
+let exportWAV = function (audioData) {
+  let encodeWAV = function (samples, sampleRate) {
+      let buffer = new ArrayBuffer(44 + samples.length * 2);
+      let view = new DataView(buffer);
+
+      let writeString = function (view, offset, string) {
+          for (let i = 0; i < string.length; i++) {
+              view.setUint8(offset + i, string.charCodeAt(i));
+          }
+      };
+
+      let floatTo16BitPCM = function (output, offset, input) {
+          for (let i = 0; i < input.length; i++, offset += 2) {
+              let s = Math.max(-1, Math.min(1, input[i]));
+              output.setInt16(offset, s < 0 ? s * 0x8000 : s * 0x7FFF, true);
+          }
+      };
+
+      writeString(view, 0, 'RIFF');  // RIFFヘッダ
+      view.setUint32(4, 32 + samples.length * 2, true); // これ以降のファイルサイズ
+      writeString(view, 8, 'WAVE'); // WAVEヘッダ
+      writeString(view, 12, 'fmt '); // fmtチャンク
+      view.setUint32(16, 16, true); // fmtチャンクのバイト数
+      view.setUint16(20, 1, true); // フォーマットID
+      view.setUint16(22, 1, true); // チャンネル数
+      view.setUint32(24, sampleRate, true); // サンプリングレート
+      view.setUint32(28, sampleRate * 2, true); // データ速度
+      view.setUint16(32, 2, true); // ブロックサイズ
+      view.setUint16(34, 16, true); // サンプルあたりのビット数
+      writeString(view, 36, 'data'); // dataチャンク
+      view.setUint32(40, samples.length * 2, true); // 波形データのバイト数
+      floatTo16BitPCM(view, 44, samples); // 波形データ
+      return view;
+  };
+
+  let mergeBuffers = function (audioData) {
+      let sampleLength = 0;
+      for (let i = 0; i < audioData.length; i++) {
+          sampleLength += audioData[i].length;
+      }
+      let samples = new Float32Array(sampleLength);
+      let sampleIdx = 0;
+      for (let i = 0; i < audioData.length; i++) {
+          for (let j = 0; j < audioData[i].length; j++) {
+              samples[sampleIdx] = audioData[i][j];
+              sampleIdx++;
+          }
+      }
+      return samples;
+  };
+
+  let dataview = encodeWAV(mergeBuffers(audioData), audio_sample_rate);
+  let audioBlob = new Blob([dataview], { type: 'audio/wav' });
+  micBlobUrl = window.URL.createObjectURL(audioBlob);
+  console.log(dataview);
+
+  let myURL = window.URL || window.webkitURL;
+  let url = myURL.createObjectURL(audioBlob);
+  download.href = url;
+};
+
+
+
+//保存
+let saveAudio = function () {
+  exportWAV(audioData);
+  download.download = 'test.wav';
+  audioContext.close().then(function () {
+  });
+};
 
 recognition.onresult = function(e){
   for (let i = e.resultIndex; (i < e.results.length); i++){
@@ -99,14 +183,6 @@ recognition.onresult = function(e){
   }
 };
 
-recognition.onend = function(){
-  if(recording){
-    recognition.start();
-  }else{
-    rec.textContent = "●";
-    mediaRecorder.stop();
-  }
-};
 
 
 
