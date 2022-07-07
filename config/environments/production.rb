ace1c08366b990c5af60c9ccd9e2a75084f6aa99
tomo-kn <okalemon314@gmail.com>
2022-07-07 17:28:48 +0900

    Fix:Uglifier::Error: Unexpected token: keyword (const). To use ES6 syntax, harmony mode must be enabled with Uglifier.new(:harmony => true)エラーの解消 #19

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,7 +29,7 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
-  config.assets.js_compressor = :uglifier
+  config.assets.js_compressor = Uglifier.new(harmony: true)
   # config.assets.digest = true
   # config.assets.resolve_with = %i[manifest]
 
