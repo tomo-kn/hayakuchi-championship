ce982782cb5df0788fb6d1e7285bf74b580f5b06
tomo-kn <okalemon314@gmail.com>
2022-07-11 19:15:56 +0900

    Feat:寿司打風タイピングゲームの実装 #20

---
+++ app/javascript/packs/game.js
@@ -0,0 +1,93 @@
+const RANDOM_SENTENCE_URL_API = "https://api.quotable.io/random";
+const typeDisplay = document.getElementById("typeDisplay");
+const typeInput = document.getElementById("typeInput");
+const timer = document.getElementById("timer");
+
+// const typeSound = new Audio("./audio/typing-sound.mp3")
+
+// inputテキスト入力。合っているかどうかの判定。
+typeInput.addEventListener("input", () => {
+  // タイプ音をつける
+  // typeSound.play();
+  // typeSound.currentTime = 0;
+
+  // console.log("a");
+  const sentenceArray = typeDisplay.querySelectorAll("span");
+  // console.log(sentenceArray);
+  const arrayValue = typeInput.value.split("");
+  // console.log(arrayValue);
+  let correct = true;
+  sentenceArray.forEach((characterSpan, index) => {
+    if (arrayValue[index] == null) {
+      characterSpan.classList.remove("correct");
+      characterSpan.classList.remove("incorrect");
+      correct = false;
+    } else if (characterSpan.innerText == arrayValue[index]) {
+      characterSpan.classList.add("correct");
+      characterSpan.classList.remove("incorrect");
+    } else {
+      characterSpan.classList.add("incorrect");
+      characterSpan.classList.remove("correct");
+
+      correct = false;
+    };
+  });
+  
+  if (correct == true) {
+    RenderNextSentence();
+  }
+});
+
+
+
+
+// 非同期でランダムな文章を取得する
+function GetRandomSentence() {
+  return fetch(RANDOM_SENTENCE_URL_API)
+  .then((response) => response.json())
+  .then((data) => data.content);
+};
+
+// ランダムな文章を取得して、表示する
+async function RenderNextSentence() {
+  const sentence = await GetRandomSentence();
+  // console.log(sentence);
+
+  typeDisplay.innerText = "";
+  //  文章を1文字ずつ分解して、spanタグを生成する
+  let oneText = sentence.split("");
+  // console.log(oneText);
+  oneText.forEach((character) => {
+    const characterSpan = document.createElement("span");
+    characterSpan.innerText = character;
+    // console.log(characterSpan);
+    typeDisplay.appendChild(characterSpan);
+    // characterSpan.classList.add("correct");
+  });
+
+  // テキストボックスの中身を消す
+  typeInput.value = "";
+
+  StartTimer();
+};
+
+let startTime;
+let originTime = 30;
+function StartTimer() {
+  timer.innerText = originTime;
+  startTime = new Date();
+  setInterval(() => {
+    timer.innerText = originTime - getTimerTime();
+    if (timer.innerText <= 0) TimeUp();
+  }, 1000);
+};
+
+function getTimerTime() {
+  return Math.floor((new Date() - startTime) / 1000);
+};
+
+function TimeUp() {
+  RenderNextSentence();
+}
+
+RenderNextSentence();
