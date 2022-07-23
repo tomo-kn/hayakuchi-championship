277078997c210ab945d4397f82b26237bc5d600e
tomo-kn <okalemon314@gmail.com>
2022-07-23 19:25:02 +0900

    Feat:usersにedit機能を追加 #27

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,6 @@
 class UsersController < ApplicationController
-  def index
-    @users = User.all
-  end
+  before_action :require_login, only: [:show, :edit, :update]
+  before_action :set_user, only: [:show, :edit, :update]
 
   def new
     @user = User.new
@@ -19,7 +18,26 @@ class UsersController < ApplicationController
     end
   end
 
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to user_path(@user)
+    else
+      render :edit
+    end
+  end
+
   private
+
+    def set_user
+      @user = User.find(current_user.id)
+    end
+
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
