c60bb54332d1236e20c649b7033763fcf5aa4a7b
tomo-kn <okalemon314@gmail.com>
2022-07-26 20:02:51 +0900

    Feat:プルダウン時にページネーションを作り直す #27

--- app/javascript/packs/mypage.js
+++ app/javascript/packs/mypage.js
@@ -11,28 +11,6 @@ $(document).on('turbolinks:load', function() {
   });
 });
 
-// プルダウンによる絞り込み
-$(".js-filter-form").on("change", function(){
-  var selected = [];
-  $(".js-filter-form :selected").each(function(){
-    selected.push($(this).val());
-  });
-  console.log(selected);
-
-  $(".js-filter-items tr").each(function(){
-    var id = $(this).data("id");
-    var is_exist = $.inArray( String(id), selected );
-    // console.log(id)
-    // console.log(selected);
-    // console.log(is_exist)
-    if (is_exist != -1) {
-      $(this).removeClass("hidden");
-    } else {
-      $(this).addClass("hidden");
-    }
-  });
-})
-
 // ページネーション
 $(".js-filter-items").pagination({
   itemElement : '> tr',
@@ -43,3 +21,42 @@ $(".js-filter-items").pagination({
   paginationClassName: 'pagination',
   setPaginationMode: 'after',
 })
+
+// プルダウンによる絞り込み
+$(".js-filter-form").on("change", function(){
+  var selected = [];
+  $(".js-filter-form :selected").each(function(){
+    selected.push($(this).val());
+  });
+  // console.log(selected);
+
+  $(".js-filter-items tr").each(function(){
+    var id = $(this).data("id");
+    var is_exist = $.inArray( String(id), selected );
+    if (selected[0] == 'all'){
+      is_exist = 0
+    }
+    // console.log(id);
+    // console.log(selected);
+    // console.log(is_exist);
+    if (is_exist != -1) {
+      $(this).removeClass("hidden");
+      $(this).css("display",""); // paginationがつけたdisplay要素を削除する
+    } else {
+      $(this).addClass("hidden");
+      $(this).css("display",""); // paginationがつけたdisplay要素を削除する
+    }
+  });
+
+  // ページネーションを作り直す
+  $('.pagination').remove();
+  $(".js-filter-items").pagination({
+    itemElement : '> tr:not(.hidden)',
+    displayItemCount: 10,
+    prevNextPageBtnMode: true,
+    firstEndPageBtnMode: true,
+    onePageOnlyDisplay: true,
+    paginationClassName: 'pagination',
+    setPaginationMode: 'after',
+  })
+});
