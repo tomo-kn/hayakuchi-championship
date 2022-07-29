d68f83bc8a515b1bb59b59b6c1df5c58eb887014
tomo-kn <okalemon314@gmail.com>
2022-07-29 18:24:01 +0900

    Feat:ランキング機能の基本を実装 #28

--- app/controllers/ranks_controller.rb
+++ app/controllers/ranks_controller.rb
@@ -1,4 +1,7 @@
 class RanksController < ApplicationController
   def index
+    query = "SELECT * FROM ( SELECT row_number() over ( partition by user_id ORDER BY `games`.`score` DESC, `games`.`out` ASC) as rownum, `games`.* FROM games ) with_rownum WHERE rownum = 1 ORDER BY `with_rownum`.`score` DESC, `with_rownum`.`out` ASC;"
+    @ranks = Game.find_by_sql(query)
+    @ranking = 1
   end
 end
