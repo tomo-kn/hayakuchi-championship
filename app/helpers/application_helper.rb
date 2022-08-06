9f9f8841dc4cf0e588c8371056a978a400003775
tomo-kn <okalemon314@gmail.com>
2022-08-06 16:59:48 +0900

    Chore:SEOとOGPのmeta_tags設定 #39

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,28 +2,30 @@ module ApplicationHelper
   def default_meta_tags
     {
       site: '早口言葉選手権',
-      title: title,
+      title: '',
       reverse: true,
       charset: 'utf-8',
-      description: '早口言葉を採点します！採点モードでライバルと競え！',
+      description: 'あなたの早口言葉を採点します。試合モードでライバルとスコアを競え！',
       keywords: '早口言葉,早口言葉選手権',
       canonical: request.original_url,
       separator: '|',
       viewport: 'width=device-width,initial-scale=1,viewport-fit=cover',
       icon: [
-        { href: image_url(''), rel: 'apple-touch-icon', sizes: '180x180', type: 'image/png' },
-        { href: image_url('favicon.ico')}
+        { href: image_url('favicon.ico')},
+        { href: image_url('icon.jpg'), rel: 'apple-touch-icon', sizes: '180x180', type: 'image/jpg' },
       ],
       og: {
-        title: :site,
+        site_name: :site,
+        title: :title,
         description: :description,
         type: 'website',
         url: request.original_url,
-        image: image_url(''),
-        locale: 'ja_JP'
+        image: image_url('hayakuchi-championship-toppage.png'),
+        locale: 'ja_JP',
       },
       twitter: {
-        card: 'summary_large_image'
+        card: 'summary_large_image',
+        site: '@tomokn5',
       }
     }
   end
