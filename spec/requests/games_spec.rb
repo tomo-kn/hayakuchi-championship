a82b6de64da8fba2bf87bb068da1263f36fb430a
tomo-kn <okalemon314@gmail.com>
2022-08-12 17:41:17 +0900

    Test:gamesコントローラーのテストを2つだけ(一旦システムテストに移る) #40

--- spec/requests/games_spec.rb
+++ spec/requests/games_spec.rb
@@ -1,4 +1,57 @@
 require 'rails_helper'
 
 RSpec.describe "Games", type: :request do
+  
+  describe "#index" do
+    # 認証済みのユーザーとして
+    context "as an authenticated user" do
+      before do
+        @user = FactoryBot.create(:user)
+      end
+      # 正常にレスポンスを返すこと
+      it "responds successfully" do
+        login @user
+        get game_path
+        expect(response).to be_successful
+        expect(response).to have_http_status "200"
+      end
+    end
+
+    # ゲストユーザーとして
+    context "as a guest" do
+      # 正常にレスポンスを返すこと
+      it "responds successfully" do
+        get game_path
+        expect(response).to be_successful
+        expect(response).to have_http_status "200"
+      end
+    end
+  end
+
+  describe "#create" do
+    # 認証済みのユーザーとして
+    context "as an authenticated user" do
+      before do
+        @user = FactoryBot.create(:user)
+      end
+    end
+
+    # ゲストユーザーとして
+    context "as a guest" do
+    end
+  end
+
+  describe "#destroy" do
+    # 認証済みのユーザーとして
+    context "as an authenticated user" do
+      before do
+        @user = FactoryBot.create(:user)
+      end
+    end
+
+    # ゲストユーザーとして
+    context "as a guest" do
+    end
+  end
+
 end
