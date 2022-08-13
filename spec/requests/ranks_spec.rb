b35003a93cdbe35c77c2a46ebe9c7c4a7d7669b3
tomo-kn <okalemon314@gmail.com>
2022-08-13 12:37:25 +0900

    Test:ranksコントローラーのRspecテスト2件 #40

--- spec/requests/ranks_spec.rb
+++ spec/requests/ranks_spec.rb
@@ -1,4 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe "Ranks", type: :request do
+
+  describe "#index" do
+    context "as an authenticated user" do
+      before do
+        @user = FactoryBot.create(:user)
+      end
+      # 正常にレスポンスを返すこと
+      it "responds successfully" do
+        login @user
+        get rank_path
+        expect(response).to be_successful
+        expect(response).to have_http_status "200"
+      end
+    end
+
+    # ゲストユーザーとして
+    context "as a guest" do
+      # 正常にレスポンスを返すこと
+      it "responds successfully" do
+        get rank_path
+        expect(response).to be_successful
+        expect(response).to have_http_status "200"
+      end
+    end
+  end
+  
 end
