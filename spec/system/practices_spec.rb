251df37343808e4fd72b67dc716c29a7ab1dd2a3
tomo-kn <okalemon314@gmail.com>
2022-08-13 21:27:59 +0900

    Test:practicesのsystemテスト2件 #40

--- spec/system/practices_spec.rb
+++ spec/system/practices_spec.rb
@@ -1,4 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe "Practices", type: :system do
+
+  before do
+    @user = FactoryBot.create(:user)
+  end
+  # 認証されたユーザーとして
+  context "as an authenticated user" do
+    # マイスコアの記述を確認する
+    scenario "user checks a myscore" do
+      login @user
+      Sentence.all.each do |sentence|
+        visit practice_path(sentence)
+        find('body').has_text?('マイスコア')
+      end
+    end
+  end
+
+  # ゲストとして
+  context "as a guest" do
+    # ログインや新規会員登録を促す記述を確認する
+    scenario "user checks a login and register recommendation" do
+      Sentence.all.each do |sentence|
+        visit practice_path(sentence)
+        expect(find('#loginAndMembership').text).to eq "ログインはこちら 新規会員登録はこちら"
+      end
+    end
+  end
 end
