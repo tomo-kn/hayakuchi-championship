9bd9732a99f585b24108b2c195f386c3136f3348
tomo-kn <okalemon314@gmail.com>
2022-08-13 21:23:15 +0900

    Test:pagesのsystemテスト2件 #40

--- spec/system/pages_spec.rb
+++ spec/system/pages_spec.rb
@@ -1,4 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe "Pages", type: :system do
+
+  before do
+    @user = FactoryBot.create(:user)
+  end
+
+  # ユーザーはログインしているときに、ログイン中の記述を確認する
+  scenario "user checks a login messsage when user logged in" do
+    login @user
+    visit root_path
+    find('body').has_text?('ログインしています')
+  end
+
+  # ユーザーは非ログイン時に、非ログインの記述を確認する
+  scenario "user checks a login recommendation when user did not log in" do
+    visit root_path
+    find('body').has_text?('ログインしていません')
+  end
 end
