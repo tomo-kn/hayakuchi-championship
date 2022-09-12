ff3ccfd964e8b4df1aa195985a985caf1a4c5443
tomo-kn <okalemon314@gmail.com>
2022-09-12 22:29:23 +0900

    Refactor:rankのsqlをgameのmodelに書き、ファットコントローラー対策 #47

--- app/controllers/ranks_controller.rb
+++ app/controllers/ranks_controller.rb
@@ -1,7 +1,6 @@
 class RanksController < ApplicationController
   def index
-    ranks_query = "SELECT rank() over (ORDER BY `with_rownum`.`score` DESC, `with_rownum`.`out` ASC) as ranking, `ranking_table`.* FROM (SELECT * FROM (SELECT row_number() over (partition by user_id ORDER BY `games`.`score` DESC, `games`.`out` ASC) as rownum, `games`.* FROM games ) with_rownum WHERE rownum = 1 ) ranking_table;"
-    @ranks = Game.find_by_sql(ranks_query)
+    @ranks = Game.make_ranking
     
     if logged_in?
       if Game.exists?(user_id: current_user.id)
--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,11 @@
 class Game < ApplicationRecord
   belongs_to :user
+
+  def self.make_ranking
+    sql = <<-EOS
+      SELECT rank() over (ORDER BY `with_rownum`.`score` DESC, `with_rownum`.`out` ASC) as ranking, `ranking_table`.* FROM (SELECT * FROM (SELECT row_number() over (partition by user_id ORDER BY `games`.`score` DESC, `games`.`out` ASC) as rownum, `games`.* FROM games ) with_rownum WHERE rownum = 1 ) ranking_table;
+    EOS
+    return Game.find_by_sql(sql)
+  end
+
 end
